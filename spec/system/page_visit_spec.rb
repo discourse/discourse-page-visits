264a0171bfd0a31c3fb1e4b4b67250fa5f3e1c3d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-06-28 11:45:07 +0200

    fix spec

--- spec/system/page_visit_spec.rb
+++ spec/system/page_visit_spec.rb
@@ -10,42 +10,38 @@ RSpec.describe "Page Visits", type: :system do
   let(:topic_page) { PageObjects::Pages::Topic.new }
 
   context "when user is authenticated" do
-    before { SiteSetting.discourse_page_visits_enabled = true }
+    before do
+      SiteSetting.discourse_page_visits_enabled = true
+      sign_in(current_user)
+    end
 
     it "creates a page visit record when user leaves the page" do
-      sign_in current_user
       topic_page.visit_topic(topic)
 
-      expect {
-        # manually trigger the visibility change event
-        page.execute_script(
-          "Object.defineProperty(document, 'visibilityState', { value: 'hidden' })",
-        )
-        page.execute_script("document.dispatchEvent(new Event('visibilitychange'))")
-        sleep 1 # wait for the page visit to be captured asynchronously
-      }.to change(DiscoursePageVisits::PageVisit, :count).by(1)
+      # manually trigger the visibility change event
+      page.execute_script("Object.defineProperty(document, 'visibilityState', { value: 'hidden' })")
+      page.execute_script("document.dispatchEvent(new Event('visibilitychange'))")
 
-      expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
-        user_id: current_user.id,
-        full_url: topic.url,
-        topic_id: topic.id,
-      )
+      try_until_success do
+        expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
+          user_id: current_user.id,
+          full_url: topic.url,
+          topic_id: topic.id,
+        )
+      end
     end
 
     it "creates a page visit record when the user changes the page" do
-      sign_in current_user
       topic_page.visit_topic(topic)
+      find("#site-logo").click
 
-      expect {
-        topic_page.visit_topic(topic_2)
-        sleep 1 # wait for the page visit to be captured asynchronously
-      }.to change(DiscoursePageVisits::PageVisit, :count).by(1)
-
-      expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
-        user_id: current_user.id,
-        full_url: topic.url,
-        topic_id: topic.id,
-      )
+      try_until_success do
+        expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
+          user_id: current_user.id,
+          full_url: topic.url,
+          topic_id: topic.id,
+        )
+      end
     end
   end
 
@@ -55,35 +51,30 @@ RSpec.describe "Page Visits", type: :system do
     it "creates a page visit record when user leaves the page" do
       topic_page.visit_topic(topic)
 
-      expect {
-        # manually trigger the visibility change event
-        page.execute_script(
-          "Object.defineProperty(document, 'visibilityState', { value: 'hidden' })",
-        )
-        page.execute_script("document.dispatchEvent(new Event('visibilitychange'))")
-        sleep 1 # wait for the page visit to be captured asynchronously
-      }.to change(DiscoursePageVisits::PageVisit, :count).by(1)
+      # manually trigger the visibility change event
+      page.execute_script("Object.defineProperty(document, 'visibilityState', { value: 'hidden' })")
+      page.execute_script("document.dispatchEvent(new Event('visibilitychange'))")
 
-      expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
-        user_id: nil,
-        full_url: topic.url,
-        topic_id: topic.id,
-      )
+      try_until_success do
+        expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
+          user_id: nil,
+          full_url: topic.url,
+          topic_id: topic.id,
+        )
+      end
     end
 
     it "creates a page visit record when the user changes the page" do
       topic_page.visit_topic(topic)
+      find("#site-logo").click
 
-      expect {
-        topic_page.visit_topic(topic_2)
-        sleep 1 # wait for the page visit to be captured asynchronously
-      }.to change(DiscoursePageVisits::PageVisit, :count).by(1)
-
-      expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
-        user_id: nil,
-        full_url: topic.url,
-        topic_id: topic.id,
-      )
+      try_until_success do
+        expect(DiscoursePageVisits::PageVisit.last).to have_attributes(
+          user_id: nil,
+          full_url: topic.url,
+          topic_id: topic.id,
+        )
+      end
     end
   end
 end

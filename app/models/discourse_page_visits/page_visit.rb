6cad855dec403bf55958d1528e11641101bf36ab
Gabriel Grubba <gabriel@discourse.org>
2024-10-24 17:02:40 -0300

    DEV: Update plugin annotations

--- app/models/discourse_page_visits/page_visit.rb
+++ app/models/discourse_page_visits/page_visit.rb
@@ -5,3 +5,19 @@ module ::DiscoursePageVisits
     self.table_name = "page_visits"
   end
 end
+
+# == Schema Information
+#
+# Table name: page_visits
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer
+#  full_url   :string           not null
+#  ip_address :string           not null
+#  user_agent :string           not null
+#  topic_id   :integer
+#  post_ids   :integer          is an Array
+#  visit_time :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
